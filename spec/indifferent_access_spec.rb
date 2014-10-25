5c3f5fa716904b8552ff721f2260a8be565b0155
Trevor Rowe <trevorrowe@gmail.com>
2014-10-24 17:45:59 -0700

    Added placeholder spec groups for testing Ruby native data structures.

---
+++ spec/indifferent_access_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+module JMESPath
+  describe '.search' do
+    describe 'indifferent access' do
+
+      describe 'Struct' do
+      end
+
+      describe 'Hash with String keys' do
+      end
+
+      describe 'Hash with Symbol keys' do
+      end
+
+      describe 'Arrays' do
+      end
+
+      describe 'Enumerable' do
+      end
+
+    end
+  end
+end
