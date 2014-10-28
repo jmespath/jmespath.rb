390075eb4d4e98fd2cb03385dabfbe76868b7a3c
Trevor Rowe <trevorrowe@gmail.com>
2014-10-27 17:13:51 -0700

    Added documentation for how to disable expression caching.

--- lib/jmespath/runtime.rb
+++ lib/jmespath/runtime.rb
@@ -5,12 +5,43 @@ module JMESPath
     # @api private
     CACHING_PARSER = CachingParser.new
 
+    # Constructs a new runtime object for evaluating JMESPath expressions.
+    #
+    #     runtime = JMESPath::Runtime.new
+    #     runtime.search(expression, data)
+    #     #=> ...
+    #
+    # ## Caching
+    #
+    # When constructing a {Runtime}, the default parser caches expressions.
+    # This significantly speeds up calls to {#search} multiple times
+    # with the same expression but different data. To disable caching, pass
+    # your own instance of {Parser}.
+    #
+    # @example Caching enabled
+    #
+    #   runtime = JMESPath::Runtime.new
+    #   runtime.parser
+    #   #=> #<JMESPath::CachingParser ...>
+    #
+    # @example Caching disabled
+    #
+    #   runtime = JMESPath::Runtime.new(parser: JMESPath::Parser.new)
+    #   runtime.parser
+    #   #=> #<JMESPath::Parser ...>
+    #
     # @option options [Parser,CachingParser] :parser
     # @option options [Interpreter] :interpreter
     def initialize(options = {})
       @parser = options[:parser] || CACHING_PARSER
       @interpreter = options[:interpreter] || TreeInterpreter.new
     end
+
+    # @return [Parser, CachingParser]
+    attr_reader :parser
+
+    # @return [Interpreter]
+    attr_reader :interpreter
 
     # @param [String<JMESPath>] expression
     # @param [Hash] data
