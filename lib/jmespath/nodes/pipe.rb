ffe3eae4e1f9335084308338efb4e405fa461f80
Theo <theo@iconara.net>
2015-01-21 14:03:17 +0100

    Make Pipe an alias for Subexpression
    They are exactly the same thing

--- lib/jmespath/nodes/pipe.rb
+++ lib/jmespath/nodes/pipe.rb
@@ -1,15 +1,6 @@
 module JMESPath
   # @api private
   module Nodes
-    class Pipe < Node
-      def initialize(left, right)
-        @left = left
-        @right = right
-      end
-
-      def visit(value)
-        @right.visit(@left.visit(value))
-      end
-    end
+    Pipe = Subexpression
   end
 end
