bdb6ef8e58173f091093210b10e1634a596ee728
Trevor Rowe <trevorrowe@gmail.com>
2016-07-18 16:01:48 -0700

    Removed simplecov usage.
    Simplecov has a direct dependency on the `json` gem which makes
    it impossible to test without the json gem present. It also forces
    a newer version (>= 1.8) which also breaks the 1.9.3 tests with
    and older json version.
    
    We can explore having a separate run of the tests to generate
    coverage, but for now, I'm removing simplecov.

--- .simplecov
+++
@@ -1,5 +0,0 @@
-SimpleCov.start do
-  project_name 'jmespath.rb'
-  add_filter '/spec/'
-  merge_timeout 60 * 15 # 15 minutes
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,2 @@
 require 'jmespath'
 require 'rspec'
-
-begin
-  require 'simplecov'
-  SimpleCov.command_name('test:unit')
-rescue LoadError
-end
-
