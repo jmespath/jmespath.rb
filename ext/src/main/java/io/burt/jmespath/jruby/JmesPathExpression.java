burtcorp/jmespath-jruby

Wrap exceptions with Ruby errors

ext/src/main/java/io/burt/jmespath/jruby/JmesPathExpression.java
@@ -2,6 +2,7 @@ package io.burt.jmespath.jruby;
 
 import org.jruby.Ruby;
 import org.jruby.RubyClass;
+import org.jruby.RubyModule;
 import org.jruby.RubyObject;
 import org.jruby.runtime.builtin.IRubyObject;
 import org.jruby.anno.JRubyClass;
@@ -10,19 +11,26 @@ import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.ObjectAllocator;
 
 import io.burt.jmespath.Expression;
+import io.burt.jmespath.function.ArityException;
+import io.burt.jmespath.function.ArgumentTypeException;
 
 @JRubyClass(name = "JmesPath::Expression")
 public class JmesPathExpression extends RubyObject {
   private final Expression<IRubyObject> expression;
+  private final RubyClass arityErrorClass;
+  private final RubyClass argumentTypeErrorClass;
 
   public JmesPathExpression(Ruby ruby, RubyClass type, Expression<IRubyObject> expression) {
     super(ruby, type);
     this.expression = expression;
+    this.arityErrorClass = (RubyClass) ruby.getClassFromPath("JmesPath::ArityError");
+    this.argumentTypeErrorClass = (RubyClass) ruby.getClassFromPath("JmesPath::ArgumentTypeError");
   }
 
-  static void install(Ruby ruby) {
-    RubyClass jmesPathExpressionClass = ruby.getClassFromPath("JmesPath").defineClassUnder("Expression", ruby.getObject(), ObjectAllocator.NOT_ALLOCATABLE_ALLOCATOR);
+  static RubyClass install(Ruby ruby, RubyModule parentModule) {
+    RubyClass jmesPathExpressionClass = parentModule.defineClassUnder("Expression", ruby.getObject(), ObjectAllocator.NOT_ALLOCATABLE_ALLOCATOR);
     jmesPathExpressionClass.defineAnnotatedMethods(JmesPathExpression.class);
+    return jmesPathExpressionClass;
   }
 
   static IRubyObject create(Ruby ruby, Expression<IRubyObject> expression) {
@@ -31,6 +39,12 @@ public class JmesPathExpression extends RubyObject {
 
   @JRubyMethod
   public IRubyObject search(ThreadContext ctx, IRubyObject target) {
-    return expression.search(target);
+    try {
+      return expression.search(target);
+    } catch (ArityException ae) {
+      throw ctx.runtime.newRaiseException(arityErrorClass, ae.getMessage());
+    } catch (ArgumentTypeException ate) {
+      throw ctx.runtime.newRaiseException(argumentTypeErrorClass, ate.getMessage());
+    }
   }
 }

spec/jmespath_spec.rb
@@ -10,18 +10,63 @@ describe JmesPath do
       expression = jmespath.compile('foo.bar')
       expect(expression).to_not be_nil
     end
+
+    context 'when there is a syntax error' do
+      it 'raises an error' do
+        expect { jmespath.compile('%') }.to raise_error(JmesPath::ParseError)
+      end
+    end
+
+    context 'when the expression refers to a function that does not exist' do
+      it 'raises an error' do
+        expect { jmespath.compile('%') }.to raise_error(JmesPath::ParseError)
+      end
+    end
+
+    context 'when the expression uses an invalid value' do
+      it 'raises an error' do
+        expect { jmespath.compile('%') }.to raise_error(JmesPath::ParseError)
+      end
+    end
   end
 end
 
 describe JmesPath::Expression do
   let :expression do
-    JmesPath.new.compile('foo.bar')
+    JmesPath.new.compile(expression_str)
+  end
+
+  let :expression_str do
+    'foo.bar'
   end
 
   describe '#search' do
+    let :input do
+      {'foo' => {'bar' => 42}}
+    end
+
     it 'searches the given input and returns the result' do
-      input = {'foo' => {'bar' => 42}}
       expect(expression.search(input)).to eq(42)
+    end
+
+    context 'when a function is given the wrong number of arguments' do
+      let :expression_str do
+        'to_number(foo, bar)'
+      end
+
+      it 'raises an error' do
+        expect { expression.search(input) }.to raise_error(JmesPath::ArityError)
+      end
+    end
+
+    context 'when a function is given the wrong kind of argument' do
+      let :expression_str do
+        'max(@)'
+      end
+
+      it 'raises an error' do
+        expect { expression.search(input) }.to raise_error(JmesPath::ArgumentTypeError)
+      end
     end
   end
 end

jmespath/jmespath.rb

ext/src/main/java/io/burt/jmespath/jruby/JmesPathExpression.java
@@ -2,6 +2,7 @@ package io.burt.jmespath.jruby;
 
 import org.jruby.Ruby;
 import org.jruby.RubyClass;
+import org.jruby.RubyModule;
 import org.jruby.RubyObject;
 import org.jruby.runtime.builtin.IRubyObject;
 import org.jruby.anno.JRubyClass;
@@ -10,19 +11,26 @@ import org.jruby.runtime.ThreadContext;
 import org.jruby.runtime.ObjectAllocator;
 
 import io.burt.jmespath.Expression;
+import io.burt.jmespath.function.ArityException;
+import io.burt.jmespath.function.ArgumentTypeException;
 
 @JRubyClass(name = "JMESPath::Expression")
 public class JmesPathExpression extends RubyObject {
   private final Expression<IRubyObject> expression;
+  private final RubyClass arityErrorClass;
+  private final RubyClass argumentTypeErrorClass;
 
   public JmesPathExpression(Ruby ruby, RubyClass type, Expression<IRubyObject> expression) {
     super(ruby, type);
     this.expression = expression;
+    this.arityErrorClass = (RubyClass) ruby.getClassFromPath("JMESPath::ArityError");
+    this.argumentTypeErrorClass = (RubyClass) ruby.getClassFromPath("JMESPath::ArgumentTypeError");
   }
 
-  static void install(Ruby ruby) {
-    RubyClass jmesPathExpressionClass = ruby.getClassFromPath("JMESPath").defineClassUnder("Expression", ruby.getObject(), ObjectAllocator.NOT_ALLOCATABLE_ALLOCATOR);
+  static RubyClass install(Ruby ruby, RubyModule parentModule) {
+    RubyClass jmesPathExpressionClass = parentModule.defineClassUnder("Expression", ruby.getObject(), ObjectAllocator.NOT_ALLOCATABLE_ALLOCATOR);
     jmesPathExpressionClass.defineAnnotatedMethods(JmesPathExpression.class);
+    return jmesPathExpressionClass;
   }
 
   static IRubyObject create(Ruby ruby, Expression<IRubyObject> expression) {
@@ -31,6 +39,12 @@ public class JmesPathExpression extends RubyObject {
 
   @JRubyMethod
   public IRubyObject search(ThreadContext ctx, IRubyObject target) {
-    return expression.search(target);
+    try {
+      return expression.search(target);
+    } catch (ArityException ae) {
+      throw ctx.runtime.newRaiseException(arityErrorClass, ae.getMessage());
+    } catch (ArgumentTypeException ate) {
+      throw ctx.runtime.newRaiseException(argumentTypeErrorClass, ate.getMessage());
+    }
   }
 }

spec/jmespath_spec.rb
@@ -11,18 +11,63 @@ if defined? JRUBY_VERSION
         expression = jmespath.compile('foo.bar')
         expect(expression).to_not be_nil
       end
+
+      context 'when there is a syntax error' do
+        it 'raises an error' do
+          expect { jmespath.compile('%') }.to raise_error(JMESPath::ParseError)
+        end
+      end
+
+      context 'when the expression refers to a function that does not exist' do
+        it 'raises an error' do
+          expect { jmespath.compile('%') }.to raise_error(JMESPath::ParseError)
+        end
+      end
+
+      context 'when the expression uses an invalid value' do
+        it 'raises an error' do
+          expect { jmespath.compile('%') }.to raise_error(JMESPath::ParseError)
+        end
+      end
     end
   end
 
   describe JMESPath::Expression do
     let :expression do
-      JMESPath.new.compile('foo.bar')
+      JMESPath.new.compile(expression_str)
+    end
+
+    let :expression_str do
+      'foo.bar'
     end
 
     describe '#search' do
+      let :input do
+        {'foo' => {'bar' => 42}}
+      end
+
       it 'searches the given input and returns the result' do
-        input = {'foo' => {'bar' => 42}}
         expect(expression.search(input)).to eq(42)
+      end
+
+      context 'when a function is given the wrong number of arguments' do
+        let :expression_str do
+          'to_number(foo, bar)'
+        end
+
+        it 'raises an error' do
+          expect { expression.search(input) }.to raise_error(JMESPath::ArityError)
+        end
+      end
+
+      context 'when a function is given the wrong kind of argument' do
+        let :expression_str do
+          'max(@)'
+        end
+
+        it 'raises an error' do
+          expect { expression.search(input) }.to raise_error(JMESPath::ArgumentTypeError)
+        end
       end
     end
   end