ef7a16e3bbae8f07518e8c143d3e5fbe6d1a64ce
Alex Woods <alexwoo@amazon.com>
2022-01-05 13:17:20 -0800

    Add compliance specs to the implicit conversion

--- spec/implicit_conversion_spec.rb
+++ spec/implicit_conversion_spec.rb
@@ -54,5 +54,33 @@ module JMESPath
       end
 
     end
+
+    describe 'Compliance' do
+      Dir.glob('spec/{compliance,legacy}/*.json').each do |path|
+
+        test_file = File.basename(path).split('.').first
+        next if test_file == 'benchmarks'
+        next if ENV['TEST_FILE'] && ENV['TEST_FILE'] != test_file
+
+        describe(test_file) do
+          JMESPath.load_json(path).each do |scenario|
+            describe("Given #{scenario['given'].to_json}") do
+              scenario['cases'].each do |test_case|
+
+                if !test_case['error']
+                  it "searching #{test_case['expression'].inspect} returns #{test_case['result'].to_json}" do
+                    result = JMESPath.search(test_case['expression'], Wrapper.wrap(scenario['given']))
+
+                    expect(JMESPath::Util.as_json(result)).to eq(test_case['result'])
+                  end
+
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
   end
 end
